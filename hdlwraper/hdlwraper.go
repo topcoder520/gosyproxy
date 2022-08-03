89a3b474c3569c0edc748ab9609872b6eb125f31
topcoder520 <38901719+topcoder520@users.noreply.github.com>
2022-08-03 16:46:50 +0800

    233

--- hdlwraper/hdlwraper.go
+++ hdlwraper/hdlwraper.go
@@ -1,7 +1,9 @@
 package hdlwraper
 
 import (
+	"bufio"
 	"io/ioutil"
+	"net"
 	"net/http"
 
 	"github.com/topcoder520/gosyproxy/mylog"
@@ -16,6 +18,23 @@ func NewHdlwraper() *Hdlwraper {
 
 func (hdl *Hdlwraper) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 	b, _ := ioutil.ReadAll(req.Body)
-	mylog.Println(req.Host)
-	mylog.Println(string(b))
+	defer req.Body.Close()
+	mylog.Println(req.URL.Scheme, req.Host, req.Method)
+	mylog.Println("body=>", string(b))
+	connIn, _, _ := resp.(http.Hijacker).Hijack()
+	defer connIn.Close()
+	connOut, _ := net.Dial("tcp", req.Host)
+	req.Header.Set("Connection", "keep-alive")
+	err := req.Write(connOut)
+	if err != nil {
+		mylog.Println("connOut=>", err)
+		return
+	}
+	buf := bufio.NewReader(connOut)
+	resp2, err := http.ReadResponse(buf, req)
+	if err != nil {
+		mylog.Println("resp2=>", err)
+		return
+	}
+	resp2.Write(connIn)
 }
