99c3f0796fe13be9f9dd2389cc63ee36210b7f22
topcoder520 <38901719+topcoder520@users.noreply.github.com>
2022-08-25 16:56:23 +0800

    add

---
+++ config/config.go
@@ -0,0 +1,56 @@
+package config
+
+import "crypto/tls"
+
+type Cfg struct {
+	Port    *string
+	Raddr   *string
+	Log     *string
+	Monitor *bool
+	Tls     *bool
+}
+
+type TlsConfig struct {
+	PrivateKeyFile  string
+	CertFile        string
+	Organization    string
+	CommonName      string
+	ServerTLSConfig *tls.Config
+}
+
+func NewTlsConfig(pk, cert, org, cn string) *TlsConfig {
+	return &TlsConfig{
+		PrivateKeyFile: pk,
+		CertFile:       cert,
+		Organization:   org,
+		CommonName:     cn,
+		ServerTLSConfig: &tls.Config{
+			CipherSuites: []uint16{
+				tls.TLS_RSA_WITH_RC4_128_SHA,
+				tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA,
+				tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+				tls.TLS_RSA_WITH_AES_128_CBC_SHA256,
+				tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_ECDSA_WITH_RC4_128_SHA,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+				tls.TLS_ECDHE_RSA_WITH_RC4_128_SHA,
+				tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+				tls.TLS_FALLBACK_SCSV,
+			},
+			PreferServerCipherSuites: true,
+		},
+	}
+}
--- hdlwraper/hdlwraper.go
+++ hdlwraper/hdlwraper.go
@@ -6,11 +6,17 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"log"
 	"net"
 	"net/http"
+	"net/http/httputil"
 	"net/url"
+	"regexp"
 	"strings"
+	"sync"
+	"time"
 
+	"github.com/topcoder520/gosyproxy/config"
 	"github.com/topcoder520/gosyproxy/mylog"
 	"github.com/topcoder520/gosyproxy/proxy"
 )
@@ -18,7 +24,18 @@ import (
 var Version = "0.1.0"
 
 type Hdlwraper struct {
-	Proxy *proxy.Proxy
+	Proxy           *proxy.Proxy
+	https           bool
+	MyConfig        *config.Cfg
+	tlsConfig       *config.TlsConfig
+	wrapped         http.Handler
+	pk              *PrivateKey
+	pkPem           []byte
+	issuingCert     *Certificate
+	issuingCertPem  []byte
+	serverTLSConfig *tls.Config
+	dynamicCerts    *Cache
+	certMutex       sync.Mutex
 }
 
 func NewHdlwraper() *Hdlwraper {
@@ -27,6 +44,141 @@ func NewHdlwraper() *Hdlwraper {
 
 func (hdl *Hdlwraper) SetProxy(proxy *proxy.Proxy) {
 	hdl.Proxy = proxy
+}
+
+func (hw *Hdlwraper) DumpHTTPAndHTTPs(resp http.ResponseWriter, req *http.Request) {
+	mylog.Println("DumpHTTPAndHTTPs")
+	req.Header.Del("Proxy-Connection")
+	req.Header.Set("Connection", "Keep-Alive")
+	var reqDump []byte
+	var err error
+	ch := make(chan bool)
+	// handle connection
+	go func() {
+		reqDump, err = httputil.DumpRequestOut(req, true)
+		ch <- true
+	}()
+	if err != nil {
+		mylog.Println("DumpRequest error ", err)
+	}
+	connIn, _, err := resp.(http.Hijacker).Hijack()
+	if err != nil {
+		mylog.Println("hijack error:", err)
+	}
+	defer connIn.Close()
+
+	var respOut *http.Response
+	host := req.Host
+
+	matched, _ := regexp.MatchString(":[0-9]+$", host)
+
+	if !hw.https {
+		if !matched {
+			host += ":80"
+		}
+
+		connOut, err := net.DialTimeout("tcp", host, time.Second*30)
+		if err != nil {
+			mylog.Println("dial to", host, "error:", err)
+			return
+		}
+
+		if err = req.Write(connOut); err != nil {
+			mylog.Println("send to server error", err)
+			return
+		}
+
+		respOut, err = http.ReadResponse(bufio.NewReader(connOut), req)
+		if err != nil && err != io.EOF {
+			mylog.Println("read response error:", err)
+		}
+
+	} else {
+		if !matched {
+			host += ":443"
+		}
+
+		connOut, err := tls.Dial("tcp", host, hw.tlsConfig.ServerTLSConfig)
+		if err != nil {
+
+		}
+		if err = req.Write(connOut); err != nil {
+			mylog.Println("tls dial to", host, "error:", err)
+			return
+		}
+		if err = req.Write(connOut); err != nil {
+			mylog.Println("send to server error", err)
+			return
+		}
+
+		respOut, err = http.ReadResponse(bufio.NewReader(connOut), req)
+		if err != nil && err != io.EOF {
+			mylog.Println("read response error:", err)
+		}
+
+	}
+
+	if respOut == nil {
+		log.Println("respOut is nil")
+		return
+	}
+
+	respDump, err := httputil.DumpResponse(respOut, true)
+	if err != nil {
+		mylog.Println("respDump error:", err)
+	}
+
+	_, err = connIn.Write(respDump)
+	if err != nil {
+		mylog.Println("connIn write error:", err)
+	}
+
+	if *hw.MyConfig.Monitor {
+		<-ch
+		go httpDump(reqDump, respOut)
+	} else {
+		<-ch
+	}
+}
+
+func (hw *Hdlwraper) InterceptHTTPs(resp http.ResponseWriter, req *http.Request) {
+	mylog.Println("InterceptHTTPs")
+	addr := req.Host
+	host := strings.Split(addr, ":")[0]
+
+	cert, err := hw.FakeCertForName(host)
+	if err != nil {
+		msg := fmt.Sprintf("Could not get mitm cert for name: %s\nerror: %s", host, err)
+		respBadGateway(resp, msg)
+		return
+	}
+
+	// handle connection
+	connIn, _, err := resp.(http.Hijacker).Hijack()
+	if err != nil {
+		msg := fmt.Sprintf("Unable to access underlying connection from client: %s", err)
+		respBadGateway(resp, msg)
+		return
+	}
+	tlsConfig := copyTlsConfig(hw.tlsConfig.ServerTLSConfig)
+	tlsConfig.Certificates = []tls.Certificate{*cert}
+	tlsConnIn := tls.Server(connIn, tlsConfig)
+	listener := &mitmListener{tlsConnIn}
+	handler := http.HandlerFunc(func(resp2 http.ResponseWriter, req2 *http.Request) {
+		req2.URL.Scheme = "https"
+		req2.URL.Host = req2.Host
+		hw.DumpHTTPAndHTTPs(resp2, req2)
+
+	})
+
+	go func() {
+		err = http.Serve(listener, handler)
+		if err != nil && err != io.EOF {
+			mylog.Printf("Error serving mitm'ed connection: %s", err)
+		}
+	}()
+
+	connIn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
 
 func (hdl *Hdlwraper) handleRequest(req *http.Request) (addr string) {
@@ -62,7 +214,20 @@ func (hdl *Hdlwraper) handleRequest(req *http.Request) (addr string) {
 }
 
 func (hdl *Hdlwraper) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+	if hdl.Proxy != nil {
+		hdl.Forward(resp, req)
+	} else {
+		if req.Method == "CONNECT" {
+			hdl.https = true
+			hdl.InterceptHTTPs(resp, req)
+		} else {
+			hdl.https = false
+			hdl.DumpHTTPAndHTTPs(resp, req)
+		}
+	}
+}
 
+func (hdl *Hdlwraper) Forward(resp http.ResponseWriter, req *http.Request) {
 	connIn, _, err := resp.(http.Hijacker).Hijack()
 	if err != nil {
 		mylog.Println("Hijack=>", err)
